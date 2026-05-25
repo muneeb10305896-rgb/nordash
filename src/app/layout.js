1aa3672b2fd7f3b6de288456ce8226d3e0f18ba6
Muneeb Ahmed Butt <muneeb10305@gmail.com>
2026-05-25 10:05:28 +0300

    fix google verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#050A14" />
-        <meta name="google-site-verification" content="d327c9614d9d1dda" />
+        <meta name="google-site-verification" content="G0tFP3VO5M1yw4IrNxt7F7QQsiOv4UsigAotNN5h8Jw" />
       </head>
       <body className={dmSans.className}>
         {children}
